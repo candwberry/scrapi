11413bbbd16dbf57f59b7cf0e19be7f9fd52989d
jamie <jammieeeeeewhite@gmail.com>
2024-07-25 12:28:06 +0100

    try this then

--- src/routes/api/amazon/+server.ts
+++ src/routes/api/amazon/+server.ts
@@ -16,6 +16,8 @@ const isWindows = process.platform === 'win32';
 
 let browser = await puppeteer.launch({
         headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+
     });
 
 export const GET: RequestHandler = async ({ request, url }) => {
