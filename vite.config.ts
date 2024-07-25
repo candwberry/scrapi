3f6f19793038c5dd729d0ce31438eec2336d1674
jamie <jammieeeeeewhite@gmail.com>
2024-07-25 13:28:13 +0100

    fix fix fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		port: 8080,
+		port: 80,
 		strictPort: false // means you can use another port if not 80
 	},
 	build: {
