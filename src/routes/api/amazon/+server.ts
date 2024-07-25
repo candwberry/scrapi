2a8be2c08194f9edbe9f89d219c0f60b358359d3
jamie <jammieeeeeewhite@gmail.com>
2024-07-25 13:08:23 +0100

    abcdefg

--- src/routes/api/amazon/+server.ts
+++ src/routes/api/amazon/+server.ts
@@ -17,7 +17,8 @@ const isWindows = process.platform === 'win32';
 let browser = await puppeteer.launch({
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        timeout: 100000
+        timeout: 100000,
+        dumpio: true,
     });
 
 export const GET: RequestHandler = async ({ request, url }) => {
