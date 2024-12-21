5d7c9478619faa97cab68a12abb6567ff1b97ae3
Jamie <jammieeeeeewhite@gmail.com>
2024-12-21 09:56:51 +0000

    Fixing null url

--- src/routes/api/db/export/+server.ts
+++ src/routes/api/db/export/+server.ts
@@ -45,7 +45,7 @@ export const GET: RequestHandler = async ({ request, url, fetch }) => {
       if (ignores.some(ignore => key.toLowerCase().includes(ignore))) {
         delete row[key];
       } else if (key.toLowerCase().includes('href')) {
-        const url = row[key];
+        const url = row[key] ?? 'null';
         delete row[key];
         row[key] = url;
         row[`${key}_domain`] = new URL(url).hostname;
@@ -164,7 +164,7 @@ WHERE p.berry IN (${berryList.map((b) => `'${b}'`).join(",")})`).all();
       if (ignores.some(ignore => key.toLowerCase().includes(ignore))) {
         delete row[key];
       } else if (key.toLowerCase().includes('href')) {
-        const url = row[key];
+        const url = row[key] ?? 'null';
         delete row[key];
         row[key] = url;
         row[`${key}_domain`] = new URL(url).hostname;
